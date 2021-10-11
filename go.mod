0795931589b722b2dd3a660a81fec513b8ad9076
abhinav-mani-dunzo <abhinav.mani@dunzo.in>
2021-10-11 13:14:33 +0530

    new relic integrated

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module Rest-api
 go 1.17
 
 require (
+	github.com/golang/protobuf v1.3.3 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.10.0 // indirect
@@ -14,8 +15,13 @@ require (
 	github.com/jackc/pgx/v4 v4.13.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.2 // indirect
+	github.com/newrelic/go-agent/v3 v3.15.0 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20190819201941-24fa4b261c55 // indirect
+	google.golang.org/grpc v1.27.0 // indirect
 	gorm.io/driver/postgres v1.1.2 // indirect
 	gorm.io/gorm v1.21.15 // indirect
 )
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"Rest-api/Database"
 	"Rest-api/controller"
 	"github.com/gorilla/mux"
+	"github.com/newrelic/go-agent/v3/newrelic"
 	"log"
 	"net/http"
 )
@@ -12,13 +13,23 @@ func main() {
 	var PORT = ":2004"
 
 	Database.Get()
+	app, err := newrelic.NewApplication(
+		newrelic.ConfigAppName("RestApi"),
+		newrelic.ConfigLicense("eu01xxd80e779f618032cd1c674822b17ceaNRAL"),
+	)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
 
 	r := mux.NewRouter()
-	r.HandleFunc("/product", controller.GetAllProduct).Methods("GET")
-	r.HandleFunc("/product/{id}", controller.GetProductById).Methods("GET")
-	r.HandleFunc("/product", controller.AddAllProduct).Methods("PUT")
-	r.HandleFunc("/topfive", controller.GetTopFiveProduct).Methods("GET")
-	r.HandleFunc("/purchase", controller.Purchase).Methods("POST")
+	r.HandleFunc(newrelic.WrapHandleFunc(app,"/product", controller.GetAllProduct)).Methods("GET")
+	r.HandleFunc(newrelic.WrapHandleFunc(app,"/product/{id}", controller.GetProductById)).Methods("GET")
+	r.HandleFunc(newrelic.WrapHandleFunc(app,"/product", controller.AddAllProduct)).Methods("PUT")
+	r.HandleFunc(newrelic.WrapHandleFunc(app,"/topfive", controller.GetTopFiveProduct)).Methods("GET")
+	r.HandleFunc(newrelic.WrapHandleFunc(app,"/purchase", controller.Purchase)).Methods("POST")
+
+
 	log.Fatalln(http.ListenAndServe(PORT, r))
 
 }
